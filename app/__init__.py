ade99a0ecda8c8eb218249e6c4ff62c2cb3be3ae
Rick <richard.mcilraith@gmail.com>
2020-06-11 19:15:41 +0100

    Started to configure the database.

--- app/__init__.py
+++ app/__init__.py
@@ -1,7 +1,11 @@
 from flask import Flask
 from config import Config
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
 
 wavefinder_app = Flask(__name__)
 wavefinder_app.config.from_object(Config)
+db = SQLAlchemy(wavefinder_app)
+migrate = Migrate(wavefinder_app, db)
 
-from app import routes
+from app import routes, models
