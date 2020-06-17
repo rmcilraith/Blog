a685144a7bae718e363392c771efef8d18ddb72b
Rick <richard.mcilraith@gmail.com>
2020-06-17 17:31:51 +0100

    Added login, logout and register

--- app/__init__.py
+++ app/__init__.py
@@ -2,10 +2,13 @@ from flask import Flask
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 
 blog_app = Flask(__name__)
 blog_app.config.from_object(Config)
 db = SQLAlchemy(blog_app)
 migrate = Migrate(blog_app, db)
+login = LoginManager(blog_app)
+login.login_view = 'login'
 
 from app import routes, models
